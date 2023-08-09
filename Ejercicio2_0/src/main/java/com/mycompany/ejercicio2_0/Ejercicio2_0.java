9b7feca291a50aca6f90fe3e617b631a3dde4307
carenMMarkevicz <markeviczcaren@gmail.com>
2023-08-09 10:59:23 -0300

    nn

---
+++ Ejercicio2_0/src/main/java/com/mycompany/ejercicio2_0/Ejercicio2_0.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.ejercicio2_0;
+
+/**
+ *
+ * @author Usuario
+ */
+public class Ejercicio2_0 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("Hola mundo!....");
+    }
+}
